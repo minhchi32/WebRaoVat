c8669cac6dd595b04b6bff4703b3c9fc5342495e
minhchi32 <tranminhchi0302@gmail.com>
2021-05-18 12:58:17 +0700

    thêm giao diện web

---
+++ WebRaoVat/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRaoVat.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult QuanLyNguoiDung()
+        {
+            return View();
+        }
+        public ActionResult QuanLyPhanQuyen()
+        {
+            return View();
+        }
+        public ActionResult QuanLyChuyenMuc()
+        {
+            return View();
+        }
+        public ActionResult QuanLyDanhMuc()
+        {
+            return View();
+        }
+        public ActionResult QuanLyBaiDang()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebRaoVat/Controllers/DangTinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRaoVat.Controllers
+{
+    public class DangTinController : Controller
+    {
+        // GET: DangTin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
